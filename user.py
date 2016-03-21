2a0edb804442e00c07ed4d5dae63b8a27fc10c4b
Carsten <info@cvantum.de>
2016-03-21 12:21:21 +0100

    Update user.py with some empty functions

--- user.py
+++ user.py
@@ -9,6 +9,7 @@ class UserMgmt(Module):
     def __init__(self):
     	# At init i have to open the user.db-File if it's exist.
     	# Else i have to create the file and init the db 
+    	db_file = '../../var/user.db'
         pass
 
     async def on_message(self, message: discord.Message):
@@ -16,11 +17,19 @@ class UserMgmt(Module):
             return False
 
         if self.has_command('whoami', message):
+            user_data = self.get_user_by_id(message.author.id)
             msg = [
             ]
             await self.container.client.send_message(message.channel, '\n'.join(msg))
 
         if self.has_command('iam', message):
+            user_data = self.get_user_by_id(message.author.id)
             msg = [
             ]
             await self.container.client.send_message(message.channel, '\n'.join(msg))
+    def get_user_by_id(self,disc_id):
+        #SELECT * FROM user WHERE id=disc_id 
+        pass
+    def get_qlstats_by_id(self,ql_id):
+        #
+        pass
